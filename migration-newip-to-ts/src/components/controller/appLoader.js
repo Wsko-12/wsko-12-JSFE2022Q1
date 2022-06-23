90ad07fbca886328ef8a9974d7913b9d921be8a1
Vlad Vasko <vaskovladislav@gmail.com>
2022-06-23 19:42:55 +0300

    feat: add api key

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'cff3ced2f2354c3a9a1ac0feb8176ccc',
         });
     }
 }
