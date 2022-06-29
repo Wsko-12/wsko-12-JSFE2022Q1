3848fbd43ced2910189dfa6f9aad527a670e1e1c
Vlad Vasko <vaskovladislav@gmail.com>
2022-06-29 15:50:40 +0300

    feat: add style and mobile adaptation

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: 'cff3ced2f2354c3a9a1ac0feb8176ccc', //a716cebce9c94d59b9158a8208390022
+            apiKey: 'a716cebce9c94d59b9158a8208390022', // cff3ced2f2354c3a9a1ac0feb8176ccc
         });
     }
 }
